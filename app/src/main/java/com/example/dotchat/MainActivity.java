120a2091c1aa343398aa246fc150f514dc185aa3
Sai Satyanarayana <Vsaisatyanarayana123@gmail.com>
2024-10-12 16:49:55 +0530

    Single commit

---
+++ app/src/main/java/com/example/dotchat/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dotchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+}
